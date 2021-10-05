64f436158b665293416a43d1a1358ed4a7c31b13
Oscar <oscar.grijalva@dt.cfe.mx>
2021-10-05 15:37:52 -0700

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,15 @@
+
+
+// Guarda en Cache Dinámico
+function actualizaCacheDinamico(dynamicCache, req, res){
+
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+
+}
